f8470a1105fe036e8d9749177ca2672d3ef97c59
Toni <keviin200garciia@gmail.com>
2021-02-15 22:49:09 -0600

    Agregar archivos de proyecto.

---
+++ TiendaBicicleta.Data/Interfaz/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using TiendaBicicleta.Models;
+
+namespace TiendaBicicleta.Data.Interfaz
+{
+    public interface IRepository<T> where T : EntityBase
+    {
+        T GetById(int Id);
+        IEnumerable<T> List();
+        IEnumerable<T> List(Expression<Func<T, bool>> predicate);
+        void Insertar(T entity);
+        void Actualizar(T entity);
+        void Borrar(T entity);
+    }
+}
